a6cd7d412ec994807a7261523203f69d3a2f9c68
Gerardo <gera_1993_06@hotmail.com>
2022-11-02 11:12:53 -0300

    Ultimos Ajustes Commit - APV_Backend

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -2,8 +2,10 @@ import nodemailer from "nodemailer";
 
 const emailOlvidePassword = async (datos) => {
     let transporter = nodemailer.createTransport({
+        pool: true,
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
@@ -14,7 +16,7 @@ const emailOlvidePassword = async (datos) => {
 
     // Enviamos el email
     const info = await transporter.sendMail({
-        from: "APV - Administrador de Pacientes de Veterinaria",
+        from: "APV - Administrador de Pacientes de Veterinaria <" + process.env.EMAIL_USER + ">",
         to: email,
         subject: "Reestablecer tu Password",
         text: "Reestablecer tu Password",
