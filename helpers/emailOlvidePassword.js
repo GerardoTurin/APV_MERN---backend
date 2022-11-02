27f7fe1da8ce19d35b13f1090796cd7bf015d287
Gerardo <gera_1993_06@hotmail.com>
2022-11-02 13:07:23 -0300

    Ultimos Ajustes Commit - APV_Backend

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -2,10 +2,8 @@ import nodemailer from "nodemailer";
 
 const emailOlvidePassword = async (datos) => {
     let transporter = nodemailer.createTransport({
-        pool: true,
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -3,10 +3,8 @@ import nodemailer from 'nodemailer';
 
 const emailRegistro = async (datos) => {
     let transporter = nodemailer.createTransport({
-        pool: true,
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: true,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
